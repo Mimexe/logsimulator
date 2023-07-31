09e6f4d33b85fb0b5a39acbc10d87d83ebe73aa4
Mime <74558118+Mimexe@users.noreply.github.com>
2023-07-31 12:41:32 +0200

    help + commands

--- README.md
+++ README.md
@@ -22,9 +22,40 @@ Then, the client will automatically start and watch for the `logs.json` file cha
 In the server, you can add logs by simply type in the terminal and hit enter.  
 To exit the server, type `/exit` and hit enter or simply hit CTRL+C.  
 The client will exit automatically when the server is closed (gracefully).  
-You can clear the client terminal with `/clear` command.
+You can clear the client terminal with `/clear` command.  
+To start the server without starting the client, use the `--no-client` flag:
+
+```sh
+node index.js --no-client
+```
+
+To start the client use the `client` argument:
+
+```sh
+node index.js client
+```
+
+Note: You need a `logs.json` file to make the client work. (without starting the server)
 
 ## Commands
 
 - `/exit`: Exit the server and the client
 - `/clear`: Clear the client terminal
+- `/help`: Show the help message
+- `/title <title>`: Change the title of the client
+
+## Troubleshooting
+
+### The client doesn't start with error 'No logs.json file found, exiting...'
+
+You need a `logs.json` file to make the client work.  
+You can create one by running the server.  
+Or you can create one manually with this content:
+
+```json
+[]
+```
+
+## License
+
+MIT License
--- index.js
+++ index.js
@@ -2,19 +2,29 @@ import { spawn } from "child_process";
 import fs from "fs";
 (async () => {
   if (process.argv[2] === "client") {
-    console.log("Starting in client mode...");
     process.title = "node index.js";
     const log = () => {
       if (!fs.existsSync("logs.json")) {
-        console.log("No logs.json file found");
         process.exit(0);
       }
       const data = fs.readFileSync("logs.json");
       const logs = JSON.parse(data);
       console.clear();
       if (logs.length === 0) return;
+      if (logs[logs.length - 1] === "[{<!%title%!>}]") {
+        const title = fs.readFileSync("title.txt").toString();
+        process.title = title;
+        fs.unlinkSync("title.txt");
+        logs.pop();
+        fs.writeFileSync("logs.json", JSON.stringify(logs));
+        return;
+      }
       console.log(logs.join("\n"));
     };
+    if (!fs.existsSync("logs.json")) {
+      console.log("No logs.json file found, exiting...");
+      process.exit(1);
+    }
     log();
     fs.watch("logs.json", log);
     return;
@@ -35,17 +45,43 @@ import fs from "fs";
       console.log("Cleared logs");
       return;
     }
+    if (log.startsWith("/title")) {
+      const title = log.split(" ").slice(1).join(" ");
+      if (!title) return console.log("No title provided");
+      fs.writeFileSync("title.txt", title);
+      logs.push(`[{<!%title%!>}]`);
+      fs.writeFileSync("logs.json", JSON.stringify(logs));
+      logs.pop();
+      console.log("Changed title");
+      return;
+    }
+    if (log === "/help") {
+      console.log("Help:");
+      console.log("/exit - Exits the server");
+      console.log("/clear - Clears the logs");
+      console.log("/title <title> - Changes the client title");
+      console.log("/help - Shows this message");
+      return;
+    }
     if (!log) return;
     logs.push(log);
-    fs.writeFileSync("logs.json", JSON.stringify(logs) + "\n");
+    fs.writeFileSync("logs.json", JSON.stringify(logs));
     console.log("Writed to logs.json");
   });
-  spawn("node", ["index.js", "client"], {
-    detached: true,
-    shell: true,
-  });
+  if (process.argv.includes("--no-client")) {
+    console.log("--no-client flag detected, not starting client");
+  } else {
+    spawn("node", ["index.js", "client"], {
+      detached: true,
+      shell: true,
+    });
+  }
   console.log("Listening for input...");
-  console.log("Type 'exit' to exit");
+  console.log("Help:");
+  console.log("/exit - Exits the server");
+  console.log("/clear - Clears the logs");
+  console.log("/title <title> - Changes the client title");
+  console.log("/help - Shows this message");
   process.stdin.resume();
   console.log("Server started");
   process.on("SIGINT", () => {
