24c5f17ba1de2090af2534b0a1257451c3746278
Mime <mime@mimedev.fr>
2024-07-25 21:58:34 +0200

    Prototype WebSocket integration

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 import { spawn } from "child_process";
 import fs from "fs";
+import io from "socket.io-client";
+import promptSync from "prompt-sync";
+const prompt = promptSync();
 (async () => {
   if (process.argv[2] === "client") {
     process.title = "node index.js";
@@ -27,6 +30,85 @@ import fs from "fs";
     }
     log();
     fs.watch("logs.json", log);
+    return;
+  } else if (process.argv[2] === "websocket") {
+    const url = process.argv[3] || "http://localhost:3000";
+    let action = prompt("Do you want to create (c) or join (j) a room? ");
+    while (!["c", "j"].includes(action)) {
+      action = prompt("Invalid option, please enter c or j: ");
+    }
+    let name = prompt("Please enter the name: ");
+    while (!name) {
+      name = prompt("Please enter the name: ");
+    }
+    let pass = prompt("Please enter the password: ", { echo: "*" });
+    while (!pass) {
+      pass = prompt("Please enter the password: ", { echo: "*" });
+    }
+    console.log("Connecting to websocket server...");
+    const socket = io(url);
+    socket.on("connect", () => {
+      console.log("Connected to websocket server");
+      socket.emit(
+        action == "c" ? "create" : "join",
+        {
+          name,
+          pass,
+        },
+        (err) => {
+          if (err) {
+            console.log("Failed to create/join room: " + err);
+            process.exit(1);
+          } else {
+            console.log("Successfully created/joined room");
+            if (action == "c") {
+              process.stdin.on("data", (data) => {
+                let log = data.toString().trim();
+                if (log === "/exit") {
+                  console.log("Exiting...");
+                  process.exit(0);
+                }
+                socket.emit("send", log);
+              });
+            }
+            if (action == "j") console.clear();
+          }
+        }
+      );
+    });
+    if (action == "c") {
+      socket.on("join", (id) => {
+        console.log("Someone (" + id + ") joined the room");
+      });
+      socket.on("leave", (id) => {
+        console.log("Someone (" + id + ") left the room");
+      });
+    } else {
+      socket.on("receive", (msg) => {
+        console.log(msg);
+      });
+    }
+    socket.on("error", (err) => {
+      console.error("Error:", err);
+    });
+    socket.on("disconnect", () => {
+      if (socket.alive) {
+        console.log("Disconnected from websocket server, reconnecting...");
+      } else {
+        console.log(
+          "Disconnected from websocket server, server has closed the connection"
+        );
+        process.exit(0);
+      }
+    });
+    socket.io.on("reconnect", () => {
+      console.log("Reconnected to websocket server");
+    });
+    socket.io.on("reconnect_attempt", (attempt) => {
+      console.log(
+        "Attempting to reconnect to websocket server (attempt " + attempt + ")"
+      );
+    });
     return;
   }
   let logs = [];
